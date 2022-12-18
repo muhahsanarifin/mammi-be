76cda79d35219ebfd84ba118e35be5d7f6818288
MUH. AHSAN <muh.ahsan.student@gmail.com>
2022-12-18 22:13:32 +0700

    Updated main.js

--- src/routes/main.js
+++ src/routes/main.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-// Import sub-routers
+// TODO: Import sub-routers
 const usersRouter = require("./users");
 const productsRouter = require("./products");
 const promosRouter = require("./promos");
@@ -11,12 +11,12 @@ const mainRouter = express.Router();
 
 const prefix = "/api/v1";
 
-// Connecting sub-routers with mainRouter
-mainRouter.use(`${prefix}/users`, usersRouter); // users endpoint handled by userRouter
-mainRouter.use(`${prefix}/products`, productsRouter); // products endpoint handled by productsRouter
-mainRouter.use(`${prefix}/promos`, promosRouter); // promos endpoint handled by promoRouter
-mainRouter.use(`${prefix}/transactions`, transactionsRouter); // transactions endpoint handled by transactionsRouter
-mainRouter.use(`${prefix}/auth`, authRouter); //auth endpoint handled by authRouter
+// TODO: Connecting sub-routers with mainRouter
+mainRouter.use(`${prefix}/users`, usersRouter); // ⇦ users endpoint handled by userRouter
+mainRouter.use(`${prefix}/products`, productsRouter); // ⇦ products endpoint handled by productsRouter
+mainRouter.use(`${prefix}/promos`, promosRouter); // ⇦ promos endpoint handled by promoRouter
+mainRouter.use(`${prefix}/transactions`, transactionsRouter); // ⇦ transactions endpoint handled by transactionsRouter
+mainRouter.use(`${prefix}/auth`, authRouter); // ⇦ auth endpoint handled by authRouter
 
 // http:localhost:8080/ ⇦ http route access
 mainRouter.get("/", (req, res) => {
